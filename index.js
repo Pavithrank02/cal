ceefff0f1e2e58501f1b12ab3217d70e4e70e79b
Pavithran2 <pavithrank02@gmail.com>
2022-12-08 09:50:49 +0530

    start

---
+++ index.js
@@ -0,0 +1,10 @@
+const val = document.getElementById('txt');
+function getValue(num) {
+  val.value += num;
+  console.log(val.value);
+}
+
+function getResult() {
+  const res = val.value + val.value
+  res.document.getElementById('txt') = val.value
+}
