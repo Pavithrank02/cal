c472a6ae643e59ac3e076b88dfe18877a0ef3388
Pavithran2 <pavithrank02@gmail.com>
2023-03-16 08:36:09 +0530

    balanace paranethesis code

--- index.js
+++ index.js
@@ -1,39 +1,128 @@
-const obj = {
-  pavi: {
-    hobbies: ["play", "write", "anime", "dead"],
-    anime: ["nauto", "baki", "AOT"]
+// const obj = {
+//   pavi: {
+//     hobbies: ["play", "write", "anime", "dead"],
+//     anime: ["nauto", "baki", "AOT"]
 
-  },
-  gowri: {
-    hobbies: [ "series","play", "anime"],
-    anime: ["nauto", "baki", "bleach"]
+//   },
+//   gowri: {
+//     hobbies: ["series", "play", "anime"],
+//     anime: ["nauto", "baki", "bleach"]
 
-  },
-  vibin: {
-    hobbies: ["play", "write", "anime"],
-    anime: ["nauto", "baki", "myhero"]
+//   },
+//   vibin: {
+//     hobbies: ["play", "write", "anime"],
+//     anime: ["nauto", "baki", "myhero"]
 
-  }
+//   }
 
+// }
+// //option 1
+// const elementCounts = {};
+// let hobbies = Object.values(obj)
+//   .map((person) => person.anime)
+//   .flat()
+
+// console.log(hobbies)
+
+// hobbies.forEach(element => {
+//   if (elementCounts[element]) {
+//     elementCounts[element] = elementCounts[element] + 1
+
+//   } else {
+//     elementCounts[element] = 1
+
+//   }
+// })
+// console.log(elementCounts);
+// const val = Object.values(elementCounts).map((series) => {
+//   return series
+// });
+// val.filter((e) => {
+//   if (e > 1) {
+//     console.log("common")
+//   } else { console.log("uncommon") }
+// })
+// console.log(val)
+
+
+
+
+
+// //console.log(arr);
+
+// //play = 1
+// //write = 111
+// //anime = 11
+// //
+// let i = 0
+// function increase(){
+//   i++
+//   console.log(`courrent counter is ${i}`)
+//   return i
+// }
+
+// increase()
+// increase()
+
+function areBracketsBalanced(expr)
+{
+      
+    // Using ArrayDeque is faster
+    // than using Stack class
+    let stack = [];
+  
+    // Traversing the Expression
+    for(let i = 0; i < expr.length; i++)
+    {
+        let x = expr[i];
+  
+        if (x == '(' || x == '[' || x == '{')
+        {
+              
+            // Push the element in the stack
+            stack.push(x);
+            continue;
+        }
+  
+        // If current character is not opening
+        // bracket, then it must be closing. 
+        // So stack cannot be empty at this point.
+        if (stack.length == 0)
+            return false;
+              
+        let check;
+        switch (x){
+        case ')':
+            check = stack.pop();
+            if (check == '{' || check == '[')
+                return false;
+            break;
+  
+        case '}':
+            check = stack.pop();
+            if (check == '(' || check == '[')
+                return false;
+            break;
+  
+        case ']':
+            check = stack.pop();
+            if (check == '(' || check == '{')
+                return false;
+            break;
+        }
+    }
+  
+    // Check Empty Stack
+    return (stack.length == 0);
 }
-//option 1
-let hobbie = Object.values(obj)
-  .map((person) => person.anime)
-  .flat();
-console.log("main",hobbie);
-
-const elementCounts = {};
-
-hobbie.forEach(element => {
-  elementCounts[element] = (elementCounts[element] || 0) + 1;
-});
-
-console.log(elementCounts);
-
-
-//console.log(arr);
-
-//play = 1
-//write = 111
-//anime = 11
-//
+  
+// Driver code
+let expr = "({}])";
+  
+// Function call
+if (areBracketsBalanced(expr))
+    document.write("Balanced ");
+else
+    document.write("Not Balanced ");
+  
+// This code is contributed by rag2127
